4f1edef482352d0ecbfc6c0799fe3e1e1ec69b78
Stephen Brown <stephen.brown@fusionalliance.com>
2017-01-16 15:33:32 -0500

    initial commit

---
+++ src/main/java/net/radmag/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package net.radmag.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by stephen.brown on 1/16/2017.
+ */
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home () {
+        return "application home";
+    }
+}
